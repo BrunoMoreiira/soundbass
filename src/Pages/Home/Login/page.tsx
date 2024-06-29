bce08b1b62f509fd3342db546d40e9b8d6dba20a
Aquiel Rocha <Aquiel123@gmail.com>
2024-06-29 20:23:43 -0300

    commit

---
+++ src/Pages/Home/Login/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import styled, { keyframes } from 'styled-components';
+
+// Keyframes para a animação de rotação
+const spinAnimation = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+// Estilo para o ícone da ampulheta
+const HourglassIcon = styled.div`
+  border: 6px solid transparent;
+  border-top: 6px solid #333;
+  border-bottom: 6px solid #333;
+  border-radius: 50%;
+  width: 50px;
+  height: 85px;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
+// Container para centralizar o ícone
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Torna o container ocupar toda a altura da tela */
+`;
+
+export default function Home() {
+  return (
+    <LoadingContainer>
+      <h1>oii</h1>
+      <HourglassIcon />
+    </LoadingContainer>
+  );
+}
---
+++ src/Pages/Home/oi.tsx
@@ -0,0 +1,12 @@
+"use client";
+  
+export default function Oi()  {
+
+<div className="nada">
+
+  <h1>como assim </h1>
+</div>
+
+
+
+}
